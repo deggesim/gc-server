5da8dee0289e467f362d6feb688888583e9a02bc
deggesim <simone.degennaro@yahoo.it>
2018-03-13 11:28:11 +0100

    Modificata porta e engines in package.json

--- src/GestioneCasa.ts
+++ src/GestioneCasa.ts
@@ -60,8 +60,8 @@ export default class GestioneCasa {
 
     public async start() {
         const app = await this.createApp();
-        console.log("Started listening on port 3000...");
-        const server = app.listen(3000);
+        console.log("Started listening on port 5000...");
+        const server = app.listen(process.env.PORT || 5000);
         return Promise.resolve(server);
     }
 
