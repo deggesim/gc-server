b2abaf77c5280712719ce769948de3b7b245c798
deggesim <simone.degennaro@yahoo.it>
2019-11-13 15:00:18 +0100

    freeRouter

--- src/gestione-casa.ts
+++ src/gestione-casa.ts
@@ -7,6 +7,8 @@ import { createConnection } from "typeorm";
 import { Inject } from "typescript-ioc";
 import Andamento from "./models/andamento";
 import TipoSpesa from "./models/tipo-spesa";
+import Token from "./models/token";
+import Utente from "./models/utente";
 import AndamentoRoutes from "./routes/andamento.routes";
 import StatisticheRoutes from "./routes/statistiche.routes";
 import TipoSpesaRoutes from "./routes/tipo-spesa.routes";
@@ -20,7 +22,6 @@ export default class GestioneCasa {
   ) { }
 
   private async createApp() {
-    const env = process.env;
     const connectionOptions = PostgressConnectionStringParser.parse(process.env.DATABASE_URL as string);
 
     await createConnection({
@@ -32,23 +33,30 @@ export default class GestioneCasa {
       database: connectionOptions.database as string,
       schema: "gc",
       entities: [
-        Andamento, TipoSpesa,
+        Andamento, TipoSpesa, Utente, Token,
       ],
       logging: true,
     });
 
     const app: Koa = new Koa();
+    const freeRouter: Router = new Router();
     const router: Router = new Router();
 
     const cors = require("@koa/cors");
     app.use(cors());
 
-    this.andamentoRoutes.register(router);
-    this.tipoSpesaRoutes.register(router);
-    this.statisticheRoutes.register(router);
+    this.andamentoRoutes.register(freeRouter, router);
+    this.tipoSpesaRoutes.register(freeRouter, router);
+    this.statisticheRoutes.register(freeRouter, router);
 
     app.use(logger());
     app.use(bodyParser());
+    app.use(freeRouter.routes());
+    app.use(freeRouter.allowedMethods());
+
+    // Middleware below this line is only reached if JWT token is valid
+    const jwt = require("koa-jwt");
+    app.use(jwt({ secret: process.env.PUBLIC_KEY }));
     app.use(router.routes());
     app.use(router.allowedMethods());
 
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -5,22 +5,25 @@ export abstract class Routes {
 
   protected abstract getRoutes(): Route[];
 
-  public register(router: Router) {
+  public register(freeRouter: Router, router: Router) {
     this.getRoutes().forEach((route) => {
-      this.registerRoute(route, router);
+      this.registerRoute(route, freeRouter, router);
     });
   }
 
-  private registerRoute = (route: Route, router: Router) => {
+  private registerRoute = (route: Route, freeRouter: Router, router: Router) => {
     switch (route.$method) {
       case ("get"):
-        router.get(route.$path, route.$action);
+        freeRouter.get(route.$path, route.$action);
         break;
       case ("post"):
         router.post(route.$path, route.$action);
         break;
       case ("put"):
         router.put(route.$path, route.$action);
+        break;
+      case ("patch"):
+        router.patch(route.$path, route.$action);
         break;
       case ("delete"):
         router.delete(route.$path, route.$action);
